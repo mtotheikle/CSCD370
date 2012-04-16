6b420e974de61024a2acb5ec555ca4c737322e1e
Michael Williams <Mike@micahel-williams.local>
2012-04-16 10:27:44 -0700

    - Add some comments

--- Lab2/src/OhmsLawCalculator.java
+++ Lab2/src/OhmsLawCalculator.java
@@ -16,15 +16,24 @@ import com.sun.tools.hat.internal.model.JavaBoolean;
 
 import static javax.swing.GroupLayout.Alignment.*;
 
+/**
+ * Ohms Law Calculator for calculating the watts
+ * 
+ * @author Michael Williams
+ */
 public class OhmsLawCalculator extends JFrame 
 {
 	private JFrame frame = null;
 	private JPanel panel;
 	
+	/**
+	 * Creates the frame and places all the elements in correct location
+	 * 
+	 * @author Michael Williams (4/12/12)
+	 */
 	public OhmsLawCalculator()
 	{
         this.setResizable(false);
-        this.setSize(250, 200);
         
 		JLabel voltsLabel = new JLabel("Volts (E):");
         final JTextField voltsTextField = new JTextField();
@@ -32,7 +41,7 @@ public class OhmsLawCalculator extends JFrame
 		JLabel resistanceLabel = new JLabel("Resistance (R):");
 		final JTextField resistanceTextField = new JTextField(10);
         
-		JLabel currentLabel = new JLabel("Current (C):");
+		JLabel currentLabel = new JLabel("Current (I):");
 		final JTextField currentTextField = new JTextField(10);
        
         JLabel titleLabel = new JLabel("Ohms Law Calculator");
@@ -45,15 +54,13 @@ public class OhmsLawCalculator extends JFrame
         JButton calcButton = new JButton("Calculate");
         JButton clearButton = new JButton("Clear");
         
+        // Try loading the image, we must make sure the image is correct
+        // Reference from http://penguin.ewu.edu/cscd370/Spr2012/Labs1/mpg_example.java.txt
         String sFileName = "circuit1.png";
         BufferedImage image = null;
 		try {
 			image = ImageIO.read(new File(sFileName));
 		} catch (IOException ex) {
-			// handle exception...
-
-			System.out.println(ex.toString());
-			System.out.println(sFileName);
 
 			JOptionPane.showMessageDialog(null, ex.toString() + " " + sFileName);
 
@@ -63,6 +70,7 @@ public class OhmsLawCalculator extends JFrame
 
 		JLabel labelPic1 = new JLabel(new ImageIcon(image));
 		
+		// Create our group layout and begin creating the horizontal and vertical layouts
         GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setAutoCreateGaps(true);
@@ -71,7 +79,7 @@ public class OhmsLawCalculator extends JFrame
         layout.setHorizontalGroup(layout.createSequentialGroup()
     		.addGroup(layout.createParallelGroup(LEADING)
 	    		.addGroup(layout.createSequentialGroup()
-	    			.addGap(100)
+	    			.addGap(120)
         			.addComponent(titleLabel)
 	    		)
     			.addGroup(layout.createSequentialGroup()
@@ -106,7 +114,7 @@ public class OhmsLawCalculator extends JFrame
         	.addGroup(layout.createParallelGroup(BASELINE)
     			.addComponent(labelPic1)
     			.addGroup(layout.createSequentialGroup()
-	    			//.addComponent(titleLabel)
+					.addGap(5)
 					.addGroup(layout.createParallelGroup(BASELINE)
 						.addComponent(voltsLabel)
 						.addComponent(voltsTextField)
@@ -125,17 +133,22 @@ public class OhmsLawCalculator extends JFrame
 					)
 				)
 			)
+			.addGap(20)
 			.addGroup(layout.createParallelGroup(BASELINE)
 				.addComponent(wattsLabel)
 				.addComponent(resultsTextField)
 			)
 		);
   
+        // Action listeners for the buttons
+        
         clearButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				// Clear all input fields
 				currentTextField.setText("");
 				resistanceTextField.setText("");
 				voltsTextField.setText("");
+				resistanceTextField.setText("");
 			}
 		});
         
@@ -145,10 +158,10 @@ public class OhmsLawCalculator extends JFrame
 				
 				if (!currentTextField.getText().equals("") 
 					&& !resistanceTextField.getText().equals("") 
-					&& voltsTextField.getText().equals("")) {
+					&& voltsTextField.getText().equals("")) { 
+					// Calculate volts 
 					
 					if (isNumeric(currentTextField.getText()) && isNumeric(resistanceTextField.getText())) {
-						// Calculate volts
 						double volts = Double.parseDouble(currentTextField.getText()) * Double.parseDouble(resistanceTextField.getText());
 						
 						double p = Double.parseDouble(currentTextField.getText()) * volts;
@@ -164,9 +177,9 @@ public class OhmsLawCalculator extends JFrame
 				} else if (currentTextField.getText().equals("") 
 					&& !resistanceTextField.getText().equals("") 
 					&& !voltsTextField.getText().equals("")) {
+					// Calculate current
 					
 					if (isNumeric(voltsTextField.getText()) && isNumeric(resistanceTextField.getText())) {
-						// Calculate current
 						double current = Double.parseDouble(voltsTextField.getText()) / Double.parseDouble(resistanceTextField.getText());
 						
 						double p = Double.parseDouble(voltsTextField.getText()) * current;
@@ -182,9 +195,9 @@ public class OhmsLawCalculator extends JFrame
 				} else if (!currentTextField.getText().equals("") 
 					&& resistanceTextField.getText().equals("") 
 					&& !voltsTextField.getText().equals("")) {
+					// Calculate resistance
 					
 					if (isNumeric(currentTextField.getText()) && isNumeric(currentTextField.getText())) {
-						// Calculate resistance
 						double resistance = Double.parseDouble(voltsTextField.getText()) / Double.parseDouble(currentTextField.getText());
 						resistanceTextField.setText(Double.toString(round(resistance, 2)));
 						
@@ -208,6 +221,12 @@ public class OhmsLawCalculator extends JFrame
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 	
+	/**
+	 * Returns true if the string value can be parsed as a double
+	 * 
+	 * @param str
+	 * @return boolean 
+	 */
 	public boolean isNumeric(String str)
 	{
 		try {
@@ -219,6 +238,15 @@ public class OhmsLawCalculator extends JFrame
 		return(true);
 	}
 	
+	/**
+	 * Rounds a double to the given number of places
+	 * 
+	 * Reference from http://penguin.ewu.edu/cscd370/Spr2012/Labs1/round.txt
+	 * 
+	 * @param val
+	 * @param plc
+	 * @return
+	 */
 	public double round(double val, int plc)
 	{
 		double pwr = Math.pow(10,plc);  
@@ -232,9 +260,7 @@ public class OhmsLawCalculator extends JFrame
     }
 
 	/**
-	 * Main method
-	 * 
-	 * Original author from linked site on class documentation
+	 * Main method, calls the OhmsLawCalculator class to create the GUI
 	 * 
 	 * @param args
 	 */
